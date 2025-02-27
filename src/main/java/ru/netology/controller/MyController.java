1240b810f62b765f730dacc89f5388a17712fff6
Gennady <g.korolew@gmail.com>
2025-02-27 18:37:03 +0400

    дописал контроллер

--- src/main/java/ru/netology/controller/MyController.java
+++ src/main/java/ru/netology/controller/MyController.java
@@ -2,10 +2,13 @@ package ru.netology.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class MyController {
+
 
     @Value("${message}")
     private String message;
